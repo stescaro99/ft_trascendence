fc62c1f3f8ed30e09953644b3bc2c7835590ca62
Sfabi28 <samuelefabi28@gmail.com>
2025-05-23 14:54:44 +0200

    2FA funzionante, da decidere la logica

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -2,6 +2,64 @@ import { FastifyRequest, FastifyReply } from 'fastify';
 import User from '../models/user';
 import bcrypt from 'bcrypt';
 import { createJWT } from '../utils/jwt';
+import speakeasy from 'speakeasy';
+import qrcode from 'qrcode';
+
+export async function generate2FA(request: FastifyRequest, reply: FastifyReply) {
+    const { nickname } = request.body as { nickname: string };
+    try {
+        const user = await User.findOne({ where: { nickname } });
+        if (!user) {
+            reply.code(404).send({ error: 'User not found' });
+            return;
+        }
+        const secret = speakeasy.generateSecret({ name: `${process.env.TFA_SECRET || '2FA_APP'} ${nickname}` });
+        user.tfa_code = secret.base32;
+        await user.save();
+
+        const otpauth_url = secret.otpauth_url;
+        if (!otpauth_url) {
+            reply.code(500).send({ error: 'Failed to generate OTP Auth URL' });
+            return;
+        }
+        const qrCode = await qrcode.toDataURL(otpauth_url);
+
+        reply.code(200).send({
+            message: '2FA secret generated',
+            qrCode,
+        });
+    }
+    catch (error) {
+        reply.code(500).send({ error: 'Failed to generate 2FA secret', details: error });
+    }
+}
+
+export async function verify2FA(request: FastifyRequest, reply: FastifyReply) {
+    const { nickname, token } = request.body as { nickname: string; token: string };
+
+    try {
+        const user = await User.findOne({ where: { nickname } });
+        if (!user || !user.tfa_code) {
+            reply.code(404).send({ error: 'User not found or 2FA not enabled' });
+            return;
+        }
+        const verified = speakeasy.totp.verify({
+            secret: user.tfa_code,
+            encoding: 'base32',
+            token,
+        });
+        
+        if (verified) {
+            reply.code(200).send({ message: '2FA verified successfully' });
+        }
+        else {
+            reply.code(401).send({ error: 'Invalid 2FA token' });
+        }
+    }
+    catch (error) {
+        reply.code(500).send({ error: 'Failed to verify 2FA', details: error });
+    }  
+}  
 
 export async function isAvailable(request: FastifyRequest, reply: FastifyReply) {
     const { field, value } = request.query as {
@@ -40,21 +98,73 @@ export async function isAvailable(request: FastifyRequest, reply: FastifyReply) 
 }
 
 export async function login(request: FastifyRequest, reply: FastifyReply) {
-    const { nickname, password } = request.query as {
+    const { nickname, password, token2FA } = request.body as {
         nickname: string;
         password: string;
+        token2FA?: string;
     };
 
     try {
-        const user = await User.findOne({ where: { nickname: nickname}});
-        if (user && await bcrypt.compare(password, user.password)) {
-            const payload = { id: user.id, nickname: user.nickname };
-            const token = createJWT(payload, '3h');
-            reply.code(200).send({ message: 'Login successful', token, user });
-        }
-        else {
+        const user = await User.findOne({ where: { nickname } });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
             reply.code(401).send({ error: 'Invalid credentials' });
+            return;
         }
+
+       if (!user.tfa_code) {
+            const issuer = 'FT_TRASCENDENCE';
+
+            const secret = speakeasy.generateSecret({
+            name: nickname,
+            issuer: issuer
+            });
+
+            user.tfa_code = secret.base32;
+            await user.save();
+
+            const label = encodeURIComponent(`${issuer}:${nickname}`);
+            const base32 = secret.base32;
+            const otpauth_url = `otpauth://totp/${label}?secret=${base32}&issuer=${issuer}`;
+
+            if (!otpauth_url) {
+                reply.code(500).send({ error: 'Failed to generate OTP Auth URL' });
+                return;
+            }
+            const qrCode = await qrcode.toDataURL(otpauth_url);
+            console.log('TOTP URI:', secret.otpauth_url);
+            console.log('issuer:', issuer);
+
+            reply.code(206).send({
+                require2FASetup: true,
+                message: '2FA setup required. Scan the QR code with Google Authenticator.',
+                qrCode,
+            });
+            return;
+        }
+
+        if (!token2FA) {
+            reply.code(401).send({ error: '2FA token required' });
+            return;
+        }
+
+        console.log('2FA token:', token2FA);
+        console.log('2FA secret:', user.tfa_code);
+
+        const verified = speakeasy.totp.verify({
+            secret: user.tfa_code,
+            encoding: 'base32',
+            token: token2FA!,
+            window: 3,
+        });
+
+        if (!verified) {
+            reply.code(401).send({ error: 'Invalid 2FA token' });
+            return;
+        }
+
+        const payload = { id: user.id, nickname: user.nickname };
+        const jwtToken = createJWT(payload);
+        reply.code(200).send({ message: 'Login successful', token: jwtToken, user });
     }
     catch (error) {
         reply.code(500).send({ error: 'Failed to login', details: error });
