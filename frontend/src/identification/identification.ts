acc27e9d00b07fc3e6a3b0c2473fb02c2fd96a27
fgori <98gori@gmail.com>
2025-05-23 14:24:39 +0200

    ddd

--- frontend/src/identification/identification.ts
+++ frontend/src/identification/identification.ts
@@ -1,5 +1,26 @@
 import autentificationHtml from './identification.html?raw';
 import '../style.css';
 
-const autentificationPage = () => autentificationHtml;
-export default autentificationPage;
+export class IdentificationPage {
+     constructor() {
+    this.render();
+    this.addEventListeners();
+  }
+
+  private render() {
+    document.body.innerHTML = autentificationHtml;
+  }
+
+  private handleSubmit(event: Event) {
+    event.preventDefault();
+    // Logica di autenticazione qui
+    alert('Form inviato!');
+  }
+
+  private addEventListeners() {
+    const form = document.querySelector('#login-form');
+    if (form) {
+      form.addEventListener('submit', this.handleSubmit.bind(this));
+    }
+  }
+}
--- frontend/src/service/user.service.ts
+++ frontend/src/service/user.service.ts
@@ -1,5 +1,33 @@
 import { User } from '../model/user.model';
+import { environment } from '../environments/environment';
+
 
 export class UserService {
-    
-};
+    private user: User | null = null;
+
+    private apiUrl = `${environment.apiUrl}/User`; 
+
+    getUser(): User | null {
+        if (localStorage.getItem('user')) {
+            this.user = JSON.parse(localStorage.getItem('user') || '');
+        } else {
+            this.user = null;
+        }
+        return this.user;
+    }
+
+    async takeUserFromApi(): Promise<any>{
+        const url = `${this.apiUrl}/GetUser`;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if(!response.ok){
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    }
+
+}
