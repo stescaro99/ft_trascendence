e345f5d3281426c2453f1b845b8c33ceca16d199
ucolla VM <ubuntu_vm@ucolla.com>
2025-06-03 17:35:21 +0200

    WIP con font e stile

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,30 +1,3 @@
-/**
- * Versione vecchia, about non funzionava.
- * Il router si aspetta una stringa con l'html dentro, ma veniva ritornata un classe intera
- */
-
-/* import homePage from './home/home';
-import autentificationPage from './identification/identification';
-
-console.log("Script caricato");
-
-const appDiv = document.getElementById('app')!;
-
-const routes: Record<string, () => string> = {
-  '/': homePage,
-  '/about': autentificationPage,
-};
-
-function router() {
-  const path = location.hash.slice(1) || '/';
-  console.log("Navigazione verso:", path);
-  const render = routes[path];
-  appDiv.innerHTML = render ? render() : `<h1>404</h1><p>Pagina non trovata</p>`;
-}
-
-window.addEventListener('hashchange', router);
-window.addEventListener('load', router); */
-
 import homePage from './home/home';
 import autentificationPage from './identification/identification';
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{html,js}",
+      "./*.html"  // if your HTML files are in root
+    ],
+    theme: ,
+    plugins: [],
+}
